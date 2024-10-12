b6536c761b2aa390d674cfe56d5b712b1f09f3fe
Minor Gordon <minorg@server.fake>
2024-10-12 18:12:12 -0400

    working on interface generator

--- generators/InterfaceTsGenerator.ts
+++ generators/InterfaceTsGenerator.ts
@@ -1,42 +1,29 @@
-import { Maybe } from "purify-ts";
-import type {
-  OptionalKind,
-  PropertySignatureStructure,
-  SourceFile,
-} from "ts-morph";
+import type { SourceFile } from "ts-morph";
 import type * as ast from "../ast/index";
 import { TsGenerator } from "./TsGenerator";
 
 export class InterfaceTsGenerator extends TsGenerator {
-  protected override addObjectType(
-    objectType: ast.ObjectType,
-    sourceFile: SourceFile,
-  ): void {
-    sourceFile.addInterface({
-      name: objectType.name.tsName,
-      properties: objectType.properties.flatMap((property) =>
-        this.propertyToPropertySignatureStructure(property).toList(),
-      ),
-    });
+  constructor(ast: ast.Ast) {
+    super(ast, new InterfaceTsGenerator.Factory());
+  }
+}
+
+export namespace InterfaceTsGenerator {
+  export class Factory extends TsGenerator.Factory {
+    override createObjectType(astType: ast.ObjectType): TsGenerator.ObjectType {
+      return new ObjectType(astType, this);
+    }
   }
 
-  private propertyToPropertySignatureStructure(
-    property: ast.Property,
-  ): Maybe<OptionalKind<PropertySignatureStructure>> {
-    const minCount = property.minCount.orDefault(0);
-    const maxCount = property.maxCount.extractNullable();
-    if (minCount === 0 && maxCount === 1) {
-      return Maybe.of({
-        name: property.name.tsName,
+  export class ObjectType extends TsGenerator.ObjectType {
+    override addStructureTo(sourceFile: SourceFile): void {
+      sourceFile.addInterface({
+        isExported: true,
+        name: this.name,
+        properties: this.properties.flatMap((property) =>
+          property.toPropertySignatureStructure().toList(),
+        ),
       });
     }
-    if (minCount === 1 && maxCount === 1) {
-      return Maybe.of({
-        name: property.name.tsName,
-      });
-    }
-    return Maybe.of({
-      name: property.name.tsName,
-    });
   }
 }
--- generators/TsGenerator.ts
+++ generators/TsGenerator.ts
@@ -1,12 +1,24 @@
-import { Project, type SourceFile, ts } from "ts-morph";
+import {
+  type OptionalKind,
+  Project,
+  type PropertySignatureStructure,
+  type SourceFile,
+  ts,
+} from "ts-morph";
 import type * as ast from "../ast";
 import ScriptTarget = ts.ScriptTarget;
+import { xsd } from "@tpluscode/rdf-ns-builders";
+import { Maybe } from "purify-ts";
+import { Memoize } from "typescript-memoize";
 
 export abstract class TsGenerator {
   private readonly project: Project;
   private readonly sourceFile: SourceFile;
 
-  constructor(private readonly ast: ast.Ast) {
+  constructor(
+    private readonly ast: ast.Ast,
+    private readonly factory: TsGenerator.Factory,
+  ) {
     this.project = new Project({
       compilerOptions: {
         target: ScriptTarget.ES2020,
@@ -17,17 +29,189 @@ export abstract class TsGenerator {
   }
 
   generate(): string {
+    this.sourceFile.addImportDeclaration({
+      moduleSpecifier: "@rdfjs/types",
+      namespaceImport: "rdfjs",
+    });
+    this.sourceFile.addImportDeclaration({
+      moduleSpecifier: "purify-ts",
+      namespaceImport: "purify",
+    });
     for (const objectType of this.ast.objectTypes) {
-      this.addObjectType(objectType, this.sourceFile);
+      this.factory.createObjectType(objectType).addStructureTo(this.sourceFile);
     }
     this.sourceFile.saveSync();
     return this.project
       .getFileSystem()
       .readFileSync(this.sourceFile.getFilePath());
   }
+}
 
-  protected abstract addObjectType(
-    objectType: ast.ObjectType,
-    sourceFile: SourceFile,
-  ): void;
+export namespace TsGenerator {
+  export abstract class Type<AstType extends ast.Type> {
+    constructor(
+      protected readonly astType: AstType,
+      protected readonly factory: Factory,
+    ) {}
+
+    abstract readonly name: string;
+  }
+
+  export class AndType extends Type<ast.AndType> {
+    get name(): string {
+      return `(${this.types.map((type) => type.name).join(" & ")})`;
+    }
+
+    @Memoize()
+    private get types(): readonly Type<ast.Type>[] {
+      return this.astType.types.map((astType) =>
+        this.factory.createType(astType),
+      );
+    }
+  }
+
+  export abstract class Factory {
+    createAndType(astType: ast.AndType): AndType {
+      return new AndType(astType, this);
+    }
+
+    createEnumType(astType: ast.EnumType): EnumType {
+      return new EnumType(astType, this);
+    }
+
+    createLiteralType(astType: ast.LiteralType): LiteralType {
+      return new LiteralType(astType, this);
+    }
+
+    createNumberType(astType: ast.LiteralType): LiteralType {
+      return new NumberType(astType, this);
+    }
+
+    abstract createObjectType(astType: ast.ObjectType): ObjectType;
+
+    createOrType(astType: ast.OrType): OrType {
+      return new OrType(astType, this);
+    }
+
+    createProperty(astProperty: ast.Property): Property {
+      return new Property(astProperty, this);
+    }
+
+    createStringType(astType: ast.LiteralType): LiteralType {
+      return new StringType(astType, this);
+    }
+
+    createType(astType: ast.Type): Type<ast.Type> {
+      switch (astType.kind) {
+        case "And":
+          return this.createAndType(astType);
+        case "Enum":
+          return this.createEnumType(astType);
+        case "Literal": {
+          const datatype = astType.datatype.orDefault(xsd.string);
+          if (datatype.equals(xsd.integer)) {
+            return this.createNumberType(astType);
+          }
+          if (datatype.equals(xsd.anyURI) || datatype.equals(xsd.string)) {
+            return this.createStringType(astType);
+          }
+          return this.createLiteralType(astType);
+        }
+        case "Object":
+          return this.createObjectType(astType);
+        case "Or":
+          return this.createOrType(astType);
+      }
+    }
+  }
+
+  export class EnumType extends Type<ast.EnumType> {
+    get name(): string {
+      throw new Error("not implemented");
+    }
+  }
+
+  export class LiteralType extends Type<ast.LiteralType> {
+    get name(): string {
+      return "rdfjs.Literal";
+    }
+  }
+
+  export abstract class ObjectType extends Type<ast.ObjectType> {
+    readonly properties: readonly Property[];
+
+    constructor(astType: ast.ObjectType, factory: Factory) {
+      super(astType, factory);
+      this.properties = this.astType.properties.map((astProperty) =>
+        this.factory.createProperty(astProperty),
+      );
+    }
+
+    abstract addStructureTo(sourceFile: SourceFile): void;
+
+    get name(): string {
+      return this.astType.name.tsName;
+    }
+  }
+
+  export class OrType extends Type<ast.OrType> {
+    get name(): string {
+      return `(${this.types.map((type) => type.name).join(" | ")})`;
+    }
+
+    @Memoize()
+    private get types(): readonly Type<ast.Type>[] {
+      return this.astType.types.map((astType) =>
+        this.factory.createType(astType),
+      );
+    }
+  }
+
+  export class Property {
+    readonly type: Type<ast.Type>;
+
+    constructor(
+      private readonly astProperty: ast.Property,
+      private readonly factory: Factory,
+    ) {
+      this.type = this.factory.createType(astProperty.type);
+    }
+
+    get name(): string {
+      return this.astProperty.name.tsName;
+    }
+
+    toPropertySignatureStructure(): Maybe<
+      OptionalKind<PropertySignatureStructure>
+    > {
+      const minCount = this.astProperty.minCount.orDefault(0);
+      const maxCount = this.astProperty.maxCount.extractNullable();
+      let type = this.type.name;
+      if (minCount === 0 && maxCount === 1) {
+        type = `purify.Maybe<${type}>`;
+      } else if (minCount === 1 && maxCount === 1) {
+      } else {
+        type = `readonly (${type})[]`;
+      }
+
+      return Maybe.of({
+        isReadonly: true,
+        name: this.name,
+        type,
+      });
+    }
+  }
+
+  // LiteralType subclasses
+  export class NumberType extends LiteralType {
+    override get name(): string {
+      return "number";
+    }
+  }
+
+  export class StringType extends LiteralType {
+    override get name(): string {
+      return "string";
+    }
+  }
 }
