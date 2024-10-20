d3ef868a355799a03dcbf5daa9c75d0eee52a231
Minor Gordon <minorg@server.fake>
2024-10-20 12:21:56 -0400

    regenerate classes

--- examples/mlm/generated/classes.ts
+++ examples/mlm/generated/classes.ts
@@ -72,12 +72,14 @@ export class LanguageModel extends MachineLearningModel {
   }
 
   override equals(other: LanguageModel): purifyHelpers.Equatable.EqualsResult {
-    return super.equals(other).chain(() =>
-      purifyHelpers.Equatable.objectEquals(this, other, {
-        contextWindow: purifyHelpers.Equatable.strictEquals,
-        maxTokenOutput: (left, right) => left.equals(right),
-      }),
-    );
+    return super
+      .equals(other)
+      .chain(() =>
+        purifyHelpers.Equatable.objectEquals(this, other, {
+          contextWindow: purifyHelpers.Equatable.strictEquals,
+          maxTokenOutput: (left, right) => left.equals(right),
+        }),
+      );
   }
 }
 
