e3a5e7899fc28ef435c42b0a1ffc8a9c96530734
Minor Gordon <minorg@server.fake>
2024-11-29 14:06:33 -0500

    convert top-level classes to interface

--- packages/shacl-ast/NodeShape.ts
+++ packages/shacl-ast/NodeShape.ts
@@ -1,40 +1,19 @@
-import { sh } from "@tpluscode/rdf-ns-builders";
 import type { Maybe } from "purify-ts";
-import type { Resource } from "rdfjs-resource";
-import type { PropertyShape } from "./PropertyShape.js";
-import { Shape } from "./Shape.js";
-import type { ShapesGraph } from "./ShapesGraph.js";
+import type { Shape } from "./Shape.js";
 
-export class NodeShape extends Shape {
-  readonly constraints: NodeShape.Constraints;
-
-  constructor(resource: Resource, shapesGraph: ShapesGraph) {
-    super(resource);
-    this.constraints = new NodeShape.Constraints(resource, shapesGraph);
-  }
-
-  override toString(): string {
-    return `NodeShape(node=${this.resource.identifier.value})`;
-  }
+export interface NodeShape<NodeShapeT, PropertyShapeT, ShapeT>
+  extends Shape<NodeShapeT, ShapeT> {
+  readonly constraints: NodeShape.Constraints<
+    NodeShapeT,
+    PropertyShapeT,
+    ShapeT
+  >;
 }
 
 export namespace NodeShape {
-  export class Constraints extends Shape.Constraints {
-    get closed(): Maybe<boolean> {
-      return this.resource
-        .value(sh.closed)
-        .chain((value) => value.toBoolean())
-        .toMaybe();
-    }
-
-    get properties(): readonly PropertyShape[] {
-      return [...this.resource.values(sh.property)].flatMap((value) =>
-        value
-          .toIdentifier()
-          .toMaybe()
-          .chain((shapeNode) => this.shapesGraph.propertyShapeByNode(shapeNode))
-          .toList(),
-      );
-    }
+  export interface Constraints<NodeShapeT, PropertyShapeT, ShapeT>
+    extends Shape.Constraints<NodeShapeT, ShapeT> {
+    readonly closed: Maybe<boolean>;
+    readonly properties: readonly PropertyShapeT[];
   }
 }
--- packages/shacl-ast/PropertyShape.ts
+++ packages/shacl-ast/PropertyShape.ts
@@ -1,80 +1,16 @@
 import type { BlankNode, Literal, NamedNode } from "@rdfjs/types";
-import { dash, sh } from "@tpluscode/rdf-ns-builders";
 import type { Maybe } from "purify-ts";
-import type { Resource } from "rdfjs-resource";
 import type { PropertyGroup } from "./PropertyGroup.js";
-import { PropertyPath } from "./PropertyPath.js";
-import { Shape } from "./Shape.js";
-import type { ShapesGraph } from "./ShapesGraph.js";
+import type { PropertyPath } from "./PropertyPath.js";
+import type { Shape } from "./Shape.js";
 
-export class PropertyShape extends Shape {
-  readonly constraints: Shape.Constraints;
-
-  constructor(
-    resource: Resource,
-    private readonly shapesGraph: ShapesGraph,
-  ) {
-    super(resource);
-    this.constraints = new PropertyShape.Constraints(resource, shapesGraph);
-  }
-
-  get defaultValue(): Maybe<BlankNode | Literal | NamedNode> {
-    return this.resource
-      .value(sh.defaultValue)
-      .map((value) => value.toTerm())
-      .toMaybe();
-  }
-
-  get editor(): Maybe<NamedNode> {
-    return this.resource
-      .value(dash.editor)
-      .chain((value) => value.toIri())
-      .toMaybe();
-  }
-
-  get group(): Maybe<PropertyGroup> {
-    return this.resource
-      .value(sh.group)
-      .chain((value) => value.toIri())
-      .toMaybe()
-      .chain((node) => this.shapesGraph.propertyGroupByNode(node));
-  }
-
-  get order(): Maybe<number> {
-    return this.resource
-      .value(sh.maxCount)
-      .chain((value) => value.toNumber())
-      .toMaybe();
-  }
-
-  get path(): PropertyPath {
-    return this.resource
-      .value(sh.path)
-      .chain((value) => value.toResource())
-      .chain(PropertyPath.fromResource)
-      .unsafeCoerce();
-  }
-
-  get singleLine(): Maybe<boolean> {
-    return this.resource
-      .value(dash.singleLine)
-      .chain((value) => value.toBoolean())
-      .toMaybe();
-  }
-
-  get viewer(): Maybe<NamedNode> {
-    return this.resource
-      .value(dash.viewer)
-      .chain((value) => value.toIri())
-      .toMaybe();
-  }
-
-  override toString(): string {
-    const keyValues: string[] = [`node=${this.resource.identifier.value}`];
-    const path = this.path;
-    if (path.kind === "PredicatePath") {
-      keyValues.push(`path=${path.iri.value}`);
-    }
-    return `PropertyShape(${keyValues.join(", ")})`;
-  }
+export interface PropertyShape<NodeShapeT, ShapeT>
+  extends Shape<NodeShapeT, ShapeT> {
+  readonly defaultValue: Maybe<BlankNode | Literal | NamedNode>;
+  readonly editor: Maybe<NamedNode>;
+  readonly group: Maybe<PropertyGroup>;
+  readonly order: Maybe<number>;
+  readonly path: PropertyPath;
+  readonly singleLine: Maybe<boolean>;
+  readonly viewer: Maybe<NamedNode>;
 }
