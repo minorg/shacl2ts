d5018443d845cf812ba01435cf5731f1ff2acab5
Minor Gordon <minorg@server.fake>
2024-11-30 17:34:16 -0500

    bump sparql-builder version

--- generate-package-files.ts
+++ generate-package-files.ts
@@ -84,7 +84,7 @@ const packages: readonly Package[] = [
   {
     dependencies: {
       external: {
-        "@kos-kit/sparql-builder": "2.0.95",
+        "@kos-kit/sparql-builder": "2.0.96",
         "@rdfjs/types": externalDependencyVersions["@rdfjs/types"],
         "@types/n3": externalDependencyVersions["@types/n3"],
         "@types/uuid": "^9.0.1",
