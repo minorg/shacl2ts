0b7967778487e8f2a2e5d3fcff8f00a1a5f64454
Minor Gordon <minorg@server.fake>
2024-12-15 11:57:51 -0500

    consolidate AST types

--- packages/compiler/ast/IdentifierType.ts
+++ packages/compiler/ast/IdentifierType.ts
@@ -1,14 +1,12 @@
-import type { NamedNode } from "@rdfjs/types";
+import type { BlankNode, NamedNode } from "@rdfjs/types";
 import type { NodeKind } from "@shaclmate/shacl-ast";
-import type { Maybe } from "purify-ts";
+import type { RdfjsTermType } from "./RdfjsTermType.js";
 
 /**
  * A type corresponding to sh:nodeKind of a blank node or IRI, and not corresponding to a node shape.
  */
-export interface IdentifierType {
-  readonly defaultValue: Maybe<NamedNode>;
-  readonly hasValue: Maybe<NamedNode>;
-  readonly in_: Maybe<readonly NamedNode[]>;
+export interface IdentifierType
+  extends RdfjsTermType<BlankNode | NamedNode, NamedNode> {
   readonly kind: "IdentifierType";
   readonly nodeKinds: Set<NodeKind.BLANK_NODE | NodeKind.IRI>;
 }
