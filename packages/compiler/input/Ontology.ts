41d34deda44b5cff0dbf44a31e2a44682e42f6ae
Minor Gordon <minorg@server.fake>
2024-12-15 14:16:31 -0500

    Ontology imports

--- packages/compiler/input/Ontology.ts
+++ packages/compiler/input/Ontology.ts
@@ -1,12 +1,19 @@
 import { RdfjsOntology } from "@shaclmate/shacl-ast";
 import type { Either, Maybe } from "purify-ts";
 import type { TsFeature, TsObjectDeclarationType } from "../enums/index.js";
+import { shaclmate } from "../vocabularies/index.js";
 import { tsFeatures } from "./tsFeatures.js";
 import { tsObjectDeclarationType } from "./tsObjectDeclarationType.js";
 
 export class Ontology extends RdfjsOntology {
   get tsFeatures(): Maybe<Set<TsFeature>> {
     return tsFeatures(this.resource);
+  }
+
+  get tsImports(): readonly string[] {
+    return this.resource
+      .values(shaclmate.tsImport)
+      .flatMap((value) => value.toString().toMaybe().toList());
   }
 
   get tsObjectDeclarationType(): Either<Error, TsObjectDeclarationType> {
